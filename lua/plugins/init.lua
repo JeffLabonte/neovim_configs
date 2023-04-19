5809c9fb357db3315ef905a00a3d7efde8cde056
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-04-19 19:23:48 -0400

    Add first batch of plugins

---
+++ lua/plugins/init.lua
@@ -0,0 +1,52 @@
+return {
+  "nvim-lua/plenary.nvim",
+  "MunifTanjim/nui.nvim",
+  {
+    "nvim-tree/nvim-web-devicons",
+    config = { default = true },
+  },
+  {
+    "nacro90/numb.nvim",
+    event = "BufReadPre",
+    config = true,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "BufReadPre",
+    config = true,
+  },
+  "wakatime/vim-wakatime",
+  "github/copilot.vim",
+  {
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+    config = true,
+  },
+  {
+    "rcarriga/nvim-notify",
+    event = "VeryLazy",
+    enabled = true,
+    config = true,
+  },
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+    config = true,
+  },
+  {
+    "TimUntersberger/neogit",
+    cmd = "Neogit",
+    config = {
+      integrations = {
+        diffview = true,
+      }
+    },
+    keys = {
+      {
+        "<leader>gs",
+        "<cmd>Neogit kind=floating<cr>",
+        desc = "Status"
+      }
+    }
+  },
+}
