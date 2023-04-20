c17d15004b59a9b9d8dd27f1ecc9066a4258567f
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-04-19 22:30:50 -0400

    Add dashboard

---
+++ lua/dashboard/init.lua
@@ -0,0 +1,61 @@
+function create_dashboard()
+  local dashboard = require("alpha.themes.dashboard")
+  dashboard.header.val = require("plugins.dashbaord.logo")["random"]
+  dashboard.section.buttons.val = {
+    dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+    dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+    dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+    dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+    dashboard.button("l", "鈴" .. " Lazy", ":Lazy<CR>"),
+    dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+  }
+  for _, button in ipairs(dashboard.section.buttons.val) do
+    button.opts.hl = "AlphaButtons"
+    button.opts.hl_shortcut = "AlphaShortcut"
+  end
+
+  dashboard.section.footer.opts.hl = "Constant"
+  dashboard.section.header.opts.hl = "AlphaHeader"
+  dashboard.section.buttons.opts.hl = "AlphaButtons"
+  dashboard.opts.layout[1].val = 0
+
+  return dashboard
+end
+
+function show_on_start(dashboard_opts)
+  if vim.o.filetype == "lazy" then
+    -- close and re-open lazy after showing alpha
+    vim.notify("Missing plugins installed", vim.log.levels.INFO, { title = "lazy.nvim" })
+    vim.cmd.close()
+    require("lazy").show()
+  end
+
+  require("alpha").setup(dashboard.opts)
+end
+
+return {
+  "goolord/alpha-nvim",
+  lazy = false,
+  config = function()
+    local dashboard = create_dashboard()
+    show_on_start(dashboard.opts)
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LazyVimStarted",
+      callback = function()
+        local version = "   v" .. vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch
+        local quote = table.concat(require("alpha.fortune")(), "\n")
+
+        local ms = (math.floor(stats.startup_time * 100 + 0.5) / 100)
+        local stats = require("lazy").stats()
+        local plugins = "⚡Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+
+        local footer = "\t" .. version .. "\t" .. plugins .. "\n" .. quote
+
+        dashboard.section.footer.val = footer
+        pcall(vim.cmd.AlphaRedraw)
+      end,
+    })
+  end,
+}
